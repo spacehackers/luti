f38ee11a7152b0b188143a45224b1296abd2d47c
lisab <lisab@scribd.com>
2019-05-19 13:33:46 -0700

    one critter, safari only

--- src/EmptyMap.js
+++ src/EmptyMap.js
@@ -1,23 +1,61 @@
 import React from "react"
-import { Map, ImageOverlay, Marker, Popup } from "react-leaflet"
-import L from "leaflet"
+import ReactDOM from "react-dom"
+import { Map, VideoOverlay, Marker, Popup } from "react-leaflet"
+import { CRS } from "leaflet"
+import Hls from "hls.js"
+import {
+  hls_config,
+  rows,
+  overlap,
+  img_width,
+  img_height,
+  base_url,
+  all_vid_names,
+  center,
+  init_zoom,
+  style
+} from "./vid_config"
 
-const bounds = [[0, 0], [50000, 50000]]
-const style = { height: "80vh", width: "75vw" }
+let all_locs = []
+let x = img_width
+let y = x * (img_height / img_width)
+for (let i = 0; i < rows; i++) {
+  for (let j = 0; j < rows; j++) {
+    all_locs.push([
+      [i * y - overlap, j * x - overlap],
+      [(i + 1) * y + overlap, (j + 1) * x + overlap]
+    ])
+  }
+}
+const bounds = all_locs[0]
 
 export default class EmptyMap extends React.Component {
+  constructor(props) {
+    super(props)
+    this.player = React.createRef()
+  }
+
   render() {
-    const init_zoom = -1
+    const loc = all_locs[0]
+    const url = base_url + all_vid_names[0]
     return (
       <Map
-        crs={L.CRS.Simple}
-        center={[0, 0]}
+        crs={CRS.Simple}
+        center={(center, loc)}
         minZoom={-1}
         minZoom={init_zoom - 1}
         maxZoom={init_zoom + 4}
         bounds={bounds}
         style={style}
-      />
+      >
+        <VideoOverlay
+          id="player_one"
+          ref={this.player}
+          url={url}
+          bounds={loc}
+          vid_config
+        />
+      </Map>
     )
   }
 }
---
+++ src/vid_config.js
@@ -0,0 +1,57 @@
+export const style = { height: "80vh", width: "75vw" }
+export const center = [0, 0]
+export const base_url = "https://s3.amazonaws.com/lifeundertheice/"
+
+export const hls_config = {
+  capLevelToPlayerSize: true,
+  maxBufferSize: 30,
+  maxBufferLength: 5
+}
+
+export const vid_config = {
+  muted: true,
+  autoplay: true,
+  loop: true
+}
+
+export const img_height = 1080
+export const img_width = 1920
+
+export const overlap = 10
+
+export const rows = 5 // square grid number of rows/columns
+
+export const all_vid_names = [
+  "112118_CanadaGlacierCryoconite1_NikonE200_10x_PinkRotifer.m3u8",
+  "112118_CanadaGlacierCryoconite1_NikonE200_10x_Tardigrade_Beginning.m3u8",
+  "112118_CanadaGlacierCryoconite1_NikonE200_10x_Tardigrade_End.m3u8",
+  "112118_CanadaGlacierCryoconite1_NikonE200_40x_TwoSpirals_02.m3u8",
+
+  "112118_CanadaGlacierCryoconite1_NikonE200_10x_PinkRotifer.m3u8",
+  "112118_CanadaGlacierCryoconite1_NikonE200_10x_Tardigrade_Beginning.m3u8",
+  "112118_CanadaGlacierCryoconite1_NikonE200_10x_Tardigrade_End.m3u8",
+  "112118_CanadaGlacierCryoconite1_NikonE200_40x_TwoSpirals_02.m3u8",
+
+  "112118_CanadaGlacierCryoconite1_NikonE200_10x_PinkRotifer.m3u8",
+  "112118_CanadaGlacierCryoconite1_NikonE200_10x_Tardigrade_Beginning.m3u8",
+  "112118_CanadaGlacierCryoconite1_NikonE200_10x_Tardigrade_End.m3u8",
+  "112118_CanadaGlacierCryoconite1_NikonE200_40x_TwoSpirals_02.m3u8",
+
+  "112118_CanadaGlacierCryoconite1_NikonE200_10x_PinkRotifer.m3u8",
+  "112118_CanadaGlacierCryoconite1_NikonE200_10x_Tardigrade_Beginning.m3u8",
+  "112118_CanadaGlacierCryoconite1_NikonE200_10x_Tardigrade_End.m3u8",
+  "112118_CanadaGlacierCryoconite1_NikonE200_40x_TwoSpirals_02.m3u8",
+
+  "112118_CanadaGlacierCryoconite1_NikonE200_10x_PinkRotifer.m3u8",
+  "112118_CanadaGlacierCryoconite1_NikonE200_10x_Tardigrade_Beginning.m3u8",
+  "112118_CanadaGlacierCryoconite1_NikonE200_10x_Tardigrade_End.m3u8",
+  "112118_CanadaGlacierCryoconite1_NikonE200_40x_TwoSpirals_02.m3u8",
+
+  "112118_CanadaGlacierCryoconite1_NikonE200_10x_PinkRotifer.m3u8",
+  "112118_CanadaGlacierCryoconite1_NikonE200_10x_Tardigrade_Beginning.m3u8",
+  "112118_CanadaGlacierCryoconite1_NikonE200_10x_Tardigrade_End.m3u8",
+  "112118_CanadaGlacierCryoconite1_NikonE200_40x_TwoSpirals_02.m3u8",
+
+  "112118_CanadaGlacierCryoconite1_NikonE200_10x_PinkRotifer.m3u8"
+]
+export const init_zoom = -1
