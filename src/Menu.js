742bf37cdcaf55b776bd32418b08838c93c433d1
lisab <lisab@scribd.com>
2019-11-10 20:20:33 -0800

    desktop menu, closes issue #27

--- src/Menu.js
+++ src/Menu.js
@@ -1,7 +1,7 @@
 import React from "react"
 import classNames from "classnames"
 import { CSSTransition } from "react-transition-group"
-import { TRANSITION_SPEED } from "./constants.js"
+import { TRANSITION_SPEED, MOBILE_BREAKPOINT } from "./constants.js"
 
 import "./Menu.scss"
 
@@ -28,9 +28,18 @@ export default class Menu extends React.Component {
     this.setState({ selected: this.props.page })
   }
 
+  isMobile() {
+    return false
+  }
+
   handleMenuClick(e, slug) {
     switch (slug) {
       case "anchor":
+        if (window.innerWidth > MOBILE_BREAKPOINT) {
+          this.setState({ selected: "home" })
+          return
+        }
+
         e.preventDefault()
         const open = !this.state.open
         this.setState({ open: open })
@@ -82,21 +91,17 @@ export default class Menu extends React.Component {
   renderItem(menuItem, key) {
     const { slug, content } = menuItem
     const hash = "" // window.location.hash
-    const selected = slug === this.state.selected
+    const selected = slug === this.state.selected ? "selected" : ""
+    const hidden = this.hidden(slug) ? "hidden" : ""
+    const href = slug === "home" || slug === "anchor" ? "/" : `/${slug}#${hash}`
 
     return (
-      <li
-        key={key}
-        className={classNames({
-          selected,
-          hidden: this.hidden(slug)
-        })}
-      >
+      <li key={key} className={`${selected} ${hidden} ${slug}`}>
         <div className="menu-item-wrapper">
           {this.renderIcon(key)}
           <span className="menu-link">
             <a
-              href={slug === "home" ? "/" : `/${slug}#${hash}`}
+              href={href}
               className={slug}
               onClick={e => {
                 this.handleMenuClick(e, slug)
--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,2 @@
-export const TRANSITION_SPEED = 100;
+export const TRANSITION_SPEED = 100
+export const MOBILE_BREAKPOINT = 780 // match in shared.scss
