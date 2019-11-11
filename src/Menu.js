c376cd10595cafaaf50eddfb02774636b03e6f33
lisab <lisab@scribd.com>
2019-11-10 21:45:09 -0800

    cleanup

--- src/Menu.js
+++ src/Menu.js
@@ -1,5 +1,4 @@
 import React from "react"
-import classNames from "classnames"
 import { CSSTransition } from "react-transition-group"
 import { TRANSITION_SPEED, MOBILE_BREAKPOINT } from "./constants.js"
 
