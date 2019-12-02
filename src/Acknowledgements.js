f61e6fa9174652f006e3ad3c4b310cc3a83096ed
Ariel Waldman <ariel@arielwaldman.com>
2019-12-01 19:36:46 -0800

    Small Acknowledgements page text update
    Updated Acknowledgements page to name Matt as "deputy web developer" and noted the music is coming soon

--- src/Acknowledgements.js
+++ src/Acknowledgements.js
@@ -56,8 +56,8 @@ const Acknowledgements = () => {
       <p>
         <strong><a href="https://twitter.com/mattb">Matt Biddulph</a></strong>
         <br />
-        for being the lead music composer and a web developer for Life Under the
-        Ice, using modular synths to set a mood for the microbes. Thanks also
+        for being the deputy web developer and music composer for Life Under the
+        Ice. The music component (launching soon, stay tuned!) will be using modular synths to set a mood for the microbes. Thanks also
         for being the best partner, cheerleader, and collaborator throughout.
       </p>
 
