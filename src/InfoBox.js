7007b69f921c5a37e9940e5b82e64f79325f6143
Lisa Ballard <lballard.cat@gmail.com>
2022-07-21 16:04:46 -0700

    refactor InfoBox to use hooks

--- src/InfoBox.js
+++ src/InfoBox.js
@@ -1,8 +1,9 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, { useEffect, useState } from "react";
 import classNames from "classnames";
-import debounce from "lodash/debounce";
+import PropTypes from "prop-types";
+
 import { CSSTransition } from "react-transition-group";
+
 import InfoButton from "./InfoButton";
 import ShareButtons from "./ShareButtons";
 
@@ -10,72 +11,50 @@ import "./InfoBox.scss";
 
 const TRANSITION_SPEED = 500;
 
-export default class InfoBox extends React.Component {
-  constructor(props) {
-    super(props);
+export default function InfoBox({ desc, displayMode, title, url }) {
+  const [hidden, setHidden] = useState(true);
 
-    this.state = {
-      hidden: true,
-    };
-
-    this.handleClick = debounce(this.handleClick, 500, {
-      leading: true,
-      trailing: false,
-    });
-  }
-
-  componentDidMount() {
+  useEffect(() => {
     const body = document.body;
     body.classList.add("overflow-hidden");
-  }
 
-  handleClick = () => {
-    this.setState(
-      (prevState) => ({ hidden: !prevState.hidden }),
-      () => {
-        this.toggleOpenClose();
-      }
-    );
-  };
-
-  toggleOpenClose = () => {
     const map = document.getElementsByClassName("leaflet-container")[0];
 
-    if (this.state.hidden) {
-      map.removeEventListener("click", this.handleClick);
-      map.removeEventListener("touchstart", this.handleClick);
-      map.removeEventListener("touchmove", this.handleClick);
+    const handleClick = () => {
+      setHidden(!hidden);
+    };
+
+    if (hidden) {
+      map.removeEventListener("click", handleClick);
+      map.removeEventListener("touchstart", handleClick);
+      map.removeEventListener("touchmove", handleClick);
       return;
     }
 
     window.setTimeout(() => {
-      map.addEventListener("click", this.handleClick);
-      map.addEventListener("touchstart", this.handleClick);
-      map.addEventListener("touchmove", this.handleClick);
+      map.addEventListener("click", handleClick);
+      map.addEventListener("touchstart", handleClick);
+      map.addEventListener("touchmove", handleClick);
     }, 100);
-  };
+  }, [hidden]);
 
-  render() {
-    const { title, desc } = this.props;
-
-    return (
-      <CSSTransition in={!this.state.hidden} timeout={TRANSITION_SPEED}>
-        <div>
-          <InfoButton
-            displayMode={this.props.displayMode}
-            handleClick={this.handleClick}
-          />
-          <div className={classNames("info-wrapper", this.props.displayMode)}>
-            <div className="info-title">
-              <h1>{title}</h1>
-            </div>
-            <div className="info-description">{desc}</div>
-            <ShareButtons url={this.props.url} />
+  return (
+    <CSSTransition in={!hidden} timeout={TRANSITION_SPEED}>
+      <div>
+        <InfoButton
+          displayMode={displayMode}
+          handleClick={() => setHidden(!hidden)}
+        />
+        <div className={classNames("info-wrapper", displayMode)}>
+          <div className="info-title">
+            <h1>{title}</h1>
           </div>
+          <div className="info-description">{desc}</div>
+          <ShareButtons url={url} />
         </div>
-      </CSSTransition>
-    );
-  }
+      </div>
+    </CSSTransition>
+  );
 }
 
 InfoBox.propTypes = {
